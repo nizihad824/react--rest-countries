2d3496aa357b27f29dae199a0f1f8c13a0b4d00d
nizihad824 <mdnurulislam97@gmail.com>
2022-03-23 11:29:03 +0100

    components inside folder structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+
+
 
 function App() {
  
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Countries></Countries>
+     
+      
+   
      
     </div>
   );
 }
-function LoadCountries(){
+
+/* function LoadCountries(){
   const [countries,setCountries]=useState([])
 
   useEffect(()=>{
@@ -23,8 +29,20 @@ function LoadCountries(){
     <div>
       <h1>Visiting every country in the world</h1>
       <h3>Available Countries:{countries.length}</h3>
+      {
+        countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
+      }
     </div>
   )
 }
+
+function Country(props){
+  return (
+    <div>
+      <h2>Name :{props.name}</h2>
+      <h4>Population :{props.population}</h4>
+    </div>
+  )
+} */
 
 export default App;
